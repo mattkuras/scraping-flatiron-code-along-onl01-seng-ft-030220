2dfc101064b8942fa34219492e90e7e5dc718bb4
Matt Kuras <mattkuras@gmail.com>
2020-03-21 17:09:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page 
-    
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry 
   end
   
   def print_courses
@@ -22,5 +23,5 @@ class Scraper
   
 end
 
-
+Scraper.new.get_page 
 
